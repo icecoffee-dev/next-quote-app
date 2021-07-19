701dac11c975d619111e7004e933fb39ee11fbae
atulit <coderatulit@gmail.com>
2021-07-19 16:52:41 +0530

    patched error

--- src/components/QuoteCard.jsx
+++ src/components/QuoteCard.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import style from "../../styles/QuoteCard.module.css";
 
-function QuoteCard({ props }) {
+function QuoteCard(props) {
+  console.log(props.data);
   return (
     <>
-      {console.log("props: ", props)}
-      <div className={style.quoteCard}>
-        <p className={style.quoteText}>{props.quote}</p>
-        <p className={style.info}>
-          <p className={style.authorName}>props.name</p>
-          <p className={style.id}>ID: {props.index}</p>
-        </p>
-      </div>
+      {props.data === undefined ? (
+        <></>
+      ) : (
+        <div className={style.quoteCard}>
+          <p className={style.quoteText}>{props.data.quote}</p>
+          <p className={style.info}>
+            <p className={style.authorName}>props.data.name</p>
+            <p className={style.id}>ID: {props.data.index}</p>
+          </p>
+        </div>
+      )}
     </>
   );
 }
--- src/components/QuoteCardsList.jsx
+++ src/components/QuoteCardsList.jsx
@@ -2,18 +2,20 @@ import React, { useEffect, useState } from "react";
 import QuoteCard from "./QuoteCard";
 
 function QuoteCardsList() {
+  const [loading, setLoading] = useState(true);
   const [quoteList, setQuoteList] = useState([]);
   useEffect(async () => {
     const data = await fetch("http://localhost:3000/api/quotes");
     const json = await data.json();
     setQuoteList(json);
+    setLoading(false);
   }, []);
   return (
     <>
-      {quoteList.length === 0 ? (
-        <h1>Loading State</h1>
+      {loading ? (
+        <h1>Loading Users</h1>
       ) : (
-        quoteList.map((data) => <QuoteCard props={data} key={data.index} />)
+        quoteList.map((data) => <QuoteCard data={data} key={data.index} />)
       )}
     </>
   );
